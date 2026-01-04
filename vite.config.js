b035a7420275528ecfd66c5b4b5f7f9d1478bd80
richard-asto <SV09681015@idat.pe>
2026-01-03 21:11:24 -0500

    7.- configuracion final

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/s17reto1bakery/',
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react()
   ],
 })
